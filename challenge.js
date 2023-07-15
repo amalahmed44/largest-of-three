710a6c475eb48a9b4414d4c298f9c6314690ed45
amalali8 <amalza958@gmail.com>
2023-07-15 03:19:20 +0300

    answer

--- challenge.js
+++ challenge.js
@@ -1,2 +1,13 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let num1 = 2
+let num2 = 5
+let num3 = 1
+
+if(num1>num2 && num1>num3){
+console.log("The largest number out of the three numbers is "+num1)
+} else if(num2 > num1 && num2 > num3){
+console.log("The largest number out of the three numbers is "+num2)
+}else if(num3>num1 && num3>num2){
+console.log("The largest number out of the three numbers is "+num3)
+}
